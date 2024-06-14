db9f9c78b74fef5ee37f5b4f1fd4b1d2cffc976b
Anim Bhuiya <anim.bhuiya@sjinnovation.com>
2024-06-14 13:19:44 +0600

    updated packages

--- src/getReviews.js
+++ src/getReviews.js
@@ -1,65 +1,160 @@
 const puppeteer  = require('puppeteer');
 
+// const getReviews = async (url, output = "json") => {
+//     output = output.toLowerCase();
+//     if (output != "json" && output != "object") {
+//         console.error('INVALID OUTPUT OPTION');
+//         return;
+//     }
+//     console.log('Launching headless chrome...');
+//     url = url.toString();
+//     const browser = await puppeteer.launch({args: ['--disabled-setuid-sandbox', '--no-sandbox']});
+//     const page = await browser.newPage();
+//     console.log('going to url');
+//     await page.goto(url);
+//     console.log(page.url);
+//     console.log('waiting for selector');
+//     await page.goto('YOUR_GOOGLE_MAPS_URL');
+//     await page.waitForNavigation({ waitUntil: 'networkidle2' }); // Waits for the network to be idle
+//     console.log('waiting for selector 2');
+//     await page.waitForSelector('.section-review-text', { timeout: 60000 });
+
+//     // await page.waitForSelector('.section-review-text');
+//     console.log('it\'s here ! now loop through data...')
+//     const data = await page.evaluate(() => {
+//         let reviewAuthorNamesClasses = document.getElementsByClassName('section-review-title');
+//         let reviewAuthorNames = [];
+//         for (let elements of reviewAuthorNamesClasses) {
+//             reviewAuthorNames.push(elements.innerText);
+//         }
+//         let datesClasses = document.getElementsByClassName('section-review-publish-date');
+//         let dates = [];
+//         for(let elements of datesClasses) {
+//             dates.push(elements.innerText);
+//         }
+
+//         let ratingsClasses = document.getElementsByClassName('section-review-stars');
+//         let ratings = [];
+//         for (let elements of ratingsClasses) {
+//             ratings.push(elements.children.length);
+//         }
+
+//         let reviewsContentClasses = document.getElementsByClassName('section-review-text');
+//         let reviewsContent = []
+//         for(let elements of reviewsContentClasses) {
+//             reviewsContent.push(elements.innerText);
+//         }
+//         return {
+//             reviewAuthorNames,
+//             dates,
+//             ratings,
+//             reviewsContent
+//         }
+//     })
+//     console.log('done ! closing browser...')
+//     browser.close();
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//         if(output === "json") {
+//             resolve(JSON.stringify(data));
+//         } else if(output === "object") {
+//             resolve(data);
+//         }
+//         if(reject) {
+//             reject({error: "error while scraping data."})
+//         }
+//     })
+    
+// };
+
 const getReviews = async (url, output = "json") => {
     output = output.toLowerCase();
-    if (output != "json" && output != "object") {
+    if (output !== "json" && output !== "object") {
         console.error('INVALID OUTPUT OPTION');
         return;
     }
+
     console.log('Launching headless chrome...');
-    url = url.toString();
-    const browser = await puppeteer.launch({args: ['--disabled-setuid-sandbox', '--no-sandbox']});
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
-    console.log('going to url');
-    await page.goto(url);
-    console.log(page.url);
-    console.log('waiting for selector');
-    await page.waitForSelector('.section-review-text');
-    console.log('it\'s here ! now loop through data...')
-    const data = await page.evaluate(() => {
-        let reviewAuthorNamesClasses = document.getElementsByClassName('section-review-title');
-        let reviewAuthorNames = [];
-        for (let elements of reviewAuthorNamesClasses) {
-            reviewAuthorNames.push(elements.innerText);
-        }
-        let datesClasses = document.getElementsByClassName('section-review-publish-date');
-        let dates = [];
-        for(let elements of datesClasses) {
-            dates.push(elements.innerText);
-        }
 
-        let ratingsClasses = document.getElementsByClassName('section-review-stars');
-        let ratings = [];
-        for (let elements of ratingsClasses) {
-            ratings.push(elements.children.length);
+    try {
+        console.log('Navigating to:', url);
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+            url = 'https://' + url; // Adjust protocol as necessary
         }
+        await page.goto(url);
 
-        let reviewsContentClasses = document.getElementsByClassName('section-review-text');
-        let reviewsContent = []
-        for(let elements of reviewsContentClasses) {
-            reviewsContent.push(elements.innerText);
-        }
-        return {
-            reviewAuthorNames,
-            dates,
-            ratings,
-            reviewsContent
-        }
-    })
-    console.log('done ! closing browser...')
-    browser.close();
-    console.log(data);
-    return new Promise((resolve, reject) => {
-        if(output === "json") {
-            resolve(JSON.stringify(data));
-        } else if(output === "object") {
-            resolve(data);
-        }
-        if(reject) {
-            reject({error: "error while scraping data."})
-        }
-    })
+        await page.waitForSelector('.section-listbox', { timeout: 120000 });
+        console.log('it\'s here ! now loop through data...')
+
+        // const data = await page.evaluate(() => {
+        //     let reviewAuthorNames = Array.from(document.querySelectorAll('.section-review-title')).map(el => el.innerText.trim());
+        //     let dates = Array.from(document.querySelectorAll('.section-review-publish-date')).map(el => el.innerText.trim());
+        //     let ratings = Array.from(document.querySelectorAll('.section-review-stars')).map(el => el.querySelector('.section-review-star-active').getAttribute('aria-label').replace(' stars', ''));
+        //     let reviewsContent = Array.from(document.querySelectorAll('.section-review-text')).map(el => el.innerText.trim());
+
+        //     return {
+        //         reviewAuthorNames,
+        //         dates,
+        //         ratings,
+        //         reviewsContent
+        //     };
+        // });
+
+        // console.log('Reviews data:', data);
+        // return output === "json" ? JSON.stringify(data) : data;
+
+
+        const data = await page.evaluate(() => {
+            let reviewAuthorNamesClasses = document.getElementsByClassName('section-review-title');
+            let reviewAuthorNames = [];
+            for (let elements of reviewAuthorNamesClasses) {
+                reviewAuthorNames.push(elements.innerText);
+            }
+            let datesClasses = document.getElementsByClassName('section-review-publish-date');
+            let dates = [];
+            for(let elements of datesClasses) {
+                dates.push(elements.innerText);
+            }
     
+            let ratingsClasses = document.getElementsByClassName('section-review-stars');
+            let ratings = [];
+            for (let elements of ratingsClasses) {
+                ratings.push(elements.children.length);
+            }
+    
+            let reviewsContentClasses = document.getElementsByClassName('section-review-text');
+            let reviewsContent = []
+            for(let elements of reviewsContentClasses) {
+                reviewsContent.push(elements.innerText);
+            }
+            return {
+                reviewAuthorNames,
+                dates,
+                ratings,
+                reviewsContent
+            }
+        })
+        console.log('done ! closing browser...')
+        browser.close();
+        console.log(data);
+        return new Promise((resolve, reject) => {
+            if(output === "json") {
+                resolve(JSON.stringify(data));
+            } else if(output === "object") {
+                resolve(data);
+            }
+            if(reject) {
+                reject({error: "error while scraping data."})
+            }
+        })
+
+    } catch (error) {
+        console.error('Error occurred while scraping:', error.message);
+    } finally {
+        await browser.close();
+    }
 };
 
 module.exports = getReviews;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ app = express();
 
 app.get('/', async (req, res) => {
     try {
-        const data = await getReviews("https://www.google.fr/maps/place/Joaillerie+Fr%C3%A9d%C3%A9ric+Parisse/@48.9597679,2.8768007,17z/data=!3m1!4b1!4m5!3m4!1s0x47e8a103de996a61:0xae40c24c34d9e2ea!8m2!3d48.9597644!4d2.8789894")
+        const data = await getReviews("https://www.google.com/maps/place/Tour+Eiffel/@48.8583736,2.292298,17z/data=!4m5!3m4!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813")
         res.json(data);
     }catch(e) {
         res.send(e);
